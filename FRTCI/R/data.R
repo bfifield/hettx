1f080676bd91e2776d750d1fe00760495648e631
Ben Fifield <benfifield@gmail.com>
2018-11-21 15:51:01 -0500

    up manual

---
+++ FRTCI/R/data.R
@@ -0,0 +1,19 @@
+#' Toy data set
+#' 
+#' This is a toy data set to illustrate the package methods.
+#'
+#' @name ToyData
+#' @docType data
+#' @format A dataframe containing 500 observations and 7 columns.
+#' @keywords dataset 
+"ToyData"
+
+#' Sample data set
+#' 
+#' This is a sample data set to illustrate the package methods.
+#'
+#' @name Penn46_ascii
+#' @docType data
+#' @format A dataframe containing 6384 observations and 12 columns.
+#' @keywords dataset 
+"Penn46_ascii"
